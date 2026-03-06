c7b95a861b9926c8f1494c5e2f8cebbcec0646cb
Khemmie-Ray <atokemmy@gmail.com>
2026-03-06 21:41:03 +0100

    mainnet setup

--- config/index.tsx
+++ config/index.tsx
@@ -1,15 +1,14 @@
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { celoSepolia } from '@reown/appkit/networks'
+import { celo } from '@reown/appkit/networks'
 
-// Get projectId from https://dashboard.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECTID!
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [celoSepolia]
+export const networks = [celo]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
